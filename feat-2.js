087ac4b5948f23b230ee8158c881730736af21b5
zkcpps <1129820657@qq.com>
2023-03-19 12:20:30 +0800

    添加feat-2

--- feat-2.js
+++ feat-2.js
@@ -1,3 +1,15 @@
 console.log("feat-2.js feat-1 change");
 console.log("feat-2 分支更改 feat-2.js文件");
 console.log("feat-1 分支更改 feat-2.js文件");
+
+const dayFormatYear = (days) => {
+  const year = days / 365;
+  const day = days % 365;
+  return year >= 1
+    ? `${Math.floor(year)}年${day > 0 ? `${day}天` : ""}`
+    : `${days}天`;
+};
+
+console.log(dayFormatYear(365));
+console.log(dayFormatYear(364));
+console.log(dayFormatYear(-1));
